37aea3817e97d05f20ce24c09deeb13e0c06003b
Wouter de Vos <wouter@codaisseur.com>
2016-02-09 11:47:17 +0100

    Genres

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160209102700) do
+ActiveRecord::Schema.define(version: 20160209104441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20160209102700) do
   create_table "actors", force: :cascade do |t|
     t.string   "name"
     t.date     "birth_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,10 @@ Actor.create([
     { name: "Kevin Spacey", birth_date: Date.parse("1964-09-24") },
     { name: "Julia Roberts", birth_date: Date.parse("1972-04-17") }
 ])
+
+Genre.destroy_all
+Genre.create([
+    { name: "Action" },
+    { name: "Comedy" },
+    { name: "Foreign" } # For Andrew :p
+])
